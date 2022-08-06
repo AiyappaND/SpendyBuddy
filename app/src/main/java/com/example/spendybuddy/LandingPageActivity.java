6f4cb52bce760e4570365c5c3fab6007c0005614
hqiuyu <huang.qi@husky.neu.edu>
2022-08-06 15:19:51 -0700

    Added Income Page

--- app/src/main/java/com/example/spendybuddy/LandingPageActivity.java
+++ app/src/main/java/com/example/spendybuddy/LandingPageActivity.java
@@ -12,6 +12,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class LandingPageActivity extends AppCompatActivity {
 
     private FloatingActionButton AddExpense;
+    private FloatingActionButton AddIncome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +21,21 @@ public class LandingPageActivity extends AppCompatActivity {
 
 
         AddExpense = findViewById(R.id.add_Expense);
+        AddIncome = findViewById(R.id.add_Income);
 
 
         AddExpense.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LandingPageActivity.this, Transaction.class);
+                startActivity(intent);
+            }
+        });
+
+        AddIncome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LandingPageActivity.this, IncomeActivity.class);
                 startActivity(intent);
             }
         });
