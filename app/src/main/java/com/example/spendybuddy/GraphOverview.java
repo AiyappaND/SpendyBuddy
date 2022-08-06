c1ba76309a5608f157b4eccae9f0f6222a7138bf
eathren <nolan.braman@gmail.com>
2022-08-06 14:11:47 -0800

    basic pie chart and list

---
+++ app/src/main/java/com/example/spendybuddy/GraphOverview.java
@@ -0,0 +1,14 @@
+package com.example.spendybuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GraphOverview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph_overview);
+    }
+}
