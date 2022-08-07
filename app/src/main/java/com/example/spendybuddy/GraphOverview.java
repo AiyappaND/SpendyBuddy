433fdc98a7cb64949bc5600262b7a3baace0538d
eathren <nolan.braman@gmail.com>
2022-08-06 20:28:56 -0800

    rough graph page with todo

--- app/src/main/java/com/example/spendybuddy/GraphOverview.java
+++ app/src/main/java/com/example/spendybuddy/GraphOverview.java
@@ -20,6 +20,35 @@ public class GraphOverview extends AppCompatActivity {
         setContentView(R.layout.activity_graph_overview);
 
         pieChart = findViewById(R.id.piechart);
-        tv1 = findViewById(R.id.)
+        tv1 = findViewById(R.id.graph_tv1);
+        tv2 = findViewById(R.id.graph_tv2);
+        tv3 = findViewById(R.id.graph_tv3);
+        tv4 = findViewById(R.id.graph_tv4);
+
+        // todo
+        // fetch all amounts, and then calculate amounts
+        // then
+        // tv.setText(Integer.toString(amount))
+        // pieChart.addPieSlice(
+        //    new PieModel(
+        //        "1",
+        //        Integer.parseInt(tvR.getText().toString()),
+        //        Color.parseColor("#FFA726")));
+        //pieChart.addPieSlice(
+        //    new PieModel(
+        //        "2",
+        //        Integer.parseInt(tvPython.getText().toString()),
+        //        Color.parseColor("#66BB6A")));
+        //pieChart.addPieSlice(
+        //    new PieModel(
+        //        "3",
+        //        Integer.parseInt(tvCPP.getText().toString()),
+        //        Color.parseColor("#EF5350")));
+        //pieChart.addPieSlice(
+        //    new PieModel(
+        //        "4",
+        //        Integer.parseInt(tvJava.getText().toString()),
+        //        Color.parseColor("#29B6F6")));
+
     }
 }
