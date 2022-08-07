dab24ca99effa5cb4c648f1d9c6fa6d3a2f6142d
eathren <nolan.braman@gmail.com>
2022-08-06 20:17:15 -0800

    fix id fields

--- app/src/main/java/com/example/spendybuddy/GraphOverview.java
+++ app/src/main/java/com/example/spendybuddy/GraphOverview.java
@@ -3,12 +3,23 @@ package com.example.spendybuddy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.eazegraph.lib.charts.PieChart;
 
 public class GraphOverview extends AppCompatActivity {
+
+    TextView tv1, tv2, tv3, tv4;
+    PieChart pieChart;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph_overview);
+
+        pieChart = findViewById(R.id.piechart);
+        tv1 = findViewById(R.id.)
     }
 }
