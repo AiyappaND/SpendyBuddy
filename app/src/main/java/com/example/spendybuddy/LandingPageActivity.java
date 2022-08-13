45a2d271ea866ce94e446d12043c5d207d5dfd55
eathren <nolan.braman@gmail.com>
2022-08-13 12:37:06 -0800

    layout fixes on rotate

--- app/src/main/java/com/example/spendybuddy/LandingPageActivity.java
+++ app/src/main/java/com/example/spendybuddy/LandingPageActivity.java
@@ -1,17 +1,29 @@
 package com.example.spendybuddy;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.spendybuddy.Transaction_be.OverviewTransactionActivity;
+import com.example.spendybuddy.data.model.Transaction;
 import com.example.spendybuddy.ui.login.LoginActivity;
+import com.example.spendybuddy.utils.RTDB;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class LandingPageActivity extends AppCompatActivity {
 
@@ -22,31 +34,33 @@ public class LandingPageActivity extends AppCompatActivity {
     private Button LogoutButton;
 
     private Button AddExpenseButton;
-
+    public DatabaseReference db;
     private Button TransactionList;
     private Button OverviewButton;
+    public TextView TotalExpensesValue;
     String username;
 
     private Button UserProfileButton;
-
-
+    RTDB rtdb;
+    double totalExpenses;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
         username = getIntent().getExtras().getString("username");
+        db = FirebaseDatabase.getInstance().getReference("transactions");
 
         AddExpense = findViewById(R.id.add_Expense);
         AddExpenseButton = findViewById(R.id.Expensebutton);
-
+        rtdb = new RTDB(username);
         FABLogout = findViewById(R.id.log_out);
         LogoutButton = findViewById(R.id.log_out_warning);
-
         UserProfileButton = findViewById(R.id.toUserProfile);
-
+        TotalExpensesValue = findViewById(R.id.expense_amount_Monthly);
         TransactionList = findViewById(R.id.toTransactionPage);
 
         OverviewButton = findViewById(R.id.overview_button);
+        getTotalMonthlyExpenses();
 
         AddExpense.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -133,6 +147,28 @@ public class LandingPageActivity extends AppCompatActivity {
         String username = intent.getStringExtra("username");
         profileIntent.putExtra("username", username);
         startActivity(profileIntent);
+    }
+
+    public void getTotalMonthlyExpenses() {
+        Query query = db.orderByChild("account_id").equalTo(username);
+        query.addValueEventListener(new ValueEventListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                totalExpenses = 0.0;
+                for (DataSnapshot snap : snapshot.getChildren()) {
+                    Transaction t = snap.getValue(Transaction.class);
+                    totalExpenses += t.getAmount();
+                }
+            TotalExpensesValue.setText("$"+String.valueOf(totalExpenses));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("Message canceled");
+            }
+        });
+
     }
 
 }
